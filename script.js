9c468daf1b814a445f54a1e153fc1deafa8ba62d
beto franca <beto2franca@gmail.com>
2023-11-14 17:44:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function adivinharNumero(){
 
     if(numero.value.length == 0){
         window.alert('Digite um numero.')
-    }else if(numero.value <1 || numero.value > 1000){
+    }else if(numero.value < 1 || numero.value > 1000){
         window.alert('Digte um número entre 1 e 1000')
     }if(numero.value == 55){
         res.innerHTML ='Você acertou!'
-    }else{
+    }
+    
+    if(numero.value != 55){
         res.innerHTML='Não é esse número!'
     }
 
